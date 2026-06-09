7b4bfc0248d8439c3dbe17b60209efa6b3e1fd8b
espacocria2027 <espacocria2027@gmail.com>
2026-06-09 16:28:19 -0300

    adiciona tabela usuarios

--- database.py
+++ database.py
@@ -4,15 +4,25 @@ def conectar():
     return sqlite3.connect("liza.db")
 
 def criar_tabela():
+
     conn = conectar()
     cursor = conn.cursor()
 
+    # Memória
     cursor.execute("""
     CREATE TABLE IF NOT EXISTS memoria (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
-        usuario TEXT,
         mensagem TEXT,
         resposta TEXT
+    )
+    """)
+
+    # Usuários
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS usuarios (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        usuario TEXT UNIQUE,
+        senha TEXT
     )
     """)
 
